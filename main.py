0174e1ed138e09a8cbcdf911cf67ac8043db5ce6
Arturo Suárez <manuel.suarez@cimat.mx>
2024-02-12 14:15:49 -0600

    Fix

--- main.py
+++ main.py
@@ -352,17 +352,18 @@ N_CLASSES = 2
 # Base DIR
 home_dir = os.path.expanduser('~')
 data_dir = os.path.join(home_dir, 'data')
-base_dir = os.path.join(data_dir, 'drone-detection-dataset')
+drone_dir = os.path.join(data_dir, 'drone-detection-dataset')
+aerial_dir = os.path.join(data_dir, 'aerial')
 # UAV Dataset pathes
-IMAGE_PATH_UAV = os.path.join(base_dir, "crops_with_target/crops_with_target")
-MASK_PATH_UAV = os.path.join(base_dir, "masks_with_target/masks_with_target")
+IMAGE_PATH_UAV = os.path.join(drone_dir, "crops_with_target/crops_with_target")
+MASK_PATH_UAV = os.path.join(drone_dir, "masks_with_target/masks_with_target")
 
-IMAGE_PATH_ALL = os.path.join(base_dir, "images_cropped/images_cropped")
-MASK_PATH_ALL = os.path.join(base_dir, "masks_cropped/masks_cropped")
+IMAGE_PATH_ALL = os.path.join(drone_dir, "images_cropped/images_cropped")
+MASK_PATH_ALL = os.path.join(drone_dir, "masks_cropped/masks_cropped")
 
 # AERIAL Dataset pathes
-IMAGE_PATH_AERIAL = os.path.join(base_dir, "aerial/images")
-MASK_PATH_AERIAL = os.path.join(base_dir, "aerial/masks")
+IMAGE_PATH_AERIAL = os.path.join(aerial_dir, "images")
+MASK_PATH_AERIAL = os.path.join(aerial_dir, "masks")
 
 # Weight path
 WEIGTH_PATH = ""
@@ -396,7 +397,7 @@ callbacks_list = [checkpoint]
 history = deeplab.fit(train_gen, steps_per_epoch=STEPS_PER_EPOCH, epochs=EPOCHS,
                         validation_data = val_gen, callbacks=callbacks_list)
 
-img_path = os.path.join(base_dir, "aerial/images/HELICOPTER_000834.png")
+img_path = os.path.join(drone_dir, "aerial/images/HELICOPTER_000834.png")
 image = cv2.imread(img_path)
 import matplotlib.pyplot as plt
 plt.figure()
